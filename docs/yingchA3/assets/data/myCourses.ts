958b1796877b46026d56ce6d32e0c4efc8d4e9d7
chenying <janicecy1217@gmail.com>
2020-07-24 22:01:26 -0400

    deployables

---
+++ docs/yingchA3/assets/data/myCourses.ts
@@ -0,0 +1,8 @@
+import Course from '../../app/courseInfo'
+
+export const MYCLASSES: Course[] = [
+    { courseCode: 100, courseTitle: "Mobile-Based Application Development", dayOfWeek: 5, profName: "Andy"},
+    { courseCode: 120, courseTitle: "Database Management", dayOfWeek: 3, profName: "Ellen"},
+    { courseCode: 130, courseTitle: "Data Structures & Algorithm", dayOfWeek: 1, profName: "David"},
+    { courseCode: 144, courseTitle: "Documentary Film: An Introduction", dayOfWeek: 4, profName: "Lucy"}
+]
