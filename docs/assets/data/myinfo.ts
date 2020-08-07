8194a99371ae9a23aaa484cb3edb81325c17cae2
chenying <janicecy1217@gmail.com>
2020-08-06 23:37:09 -0400

    new docs

---
+++ docs/assets/data/myinfo.ts
@@ -0,0 +1,9 @@
+import YingCh from '../../app/yingch'
+
+export const yingch: YingCh = {
+    studentId: 991549628,
+    name: 'Ying Chen',
+    loginName: 'yingch',
+    campus: 'Davis',
+    assignmentTitle: 'Assignment4'
+  }
